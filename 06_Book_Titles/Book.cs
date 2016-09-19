4a7e56f1ca37f274e5c7f8efdea4be941681a36b
davi7725 <davi7725@edu.eal.dk>
2016-09-19 10:50:53 +0200

    Add project files.

---
+++ 06_Book_Titles/Book.cs
@@ -0,0 +1,65 @@
+﻿namespace _06_Book_Titles
+{
+    internal class Book
+    {
+        private string wordsToReturn = "";
+        public string Title
+        {
+            get
+            {
+                return wordsToReturn;
+            }
+            internal set
+            {
+                string[] words = value.Split();
+                int counter = 0;
+
+                foreach(string word in words)
+                {
+                    if(0 != counter)
+                    {
+                        if ("the" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word;
+                        }
+                        else if ("a" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word;
+                        }
+                        else if ("an" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word;
+                        }
+                        else if ("and" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word;
+                        }
+                        else if ("in" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word;
+                        }
+                        else if ("of" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word;
+                        }
+                        else if ("i" == word)
+                        {
+                            wordsToReturn = wordsToReturn + " " + word.ToUpper();
+                        }
+                        else
+                        {
+                            wordsToReturn = wordsToReturn + " " + word.Substring(0, 1).ToUpper() + word.Substring(1);
+                        }
+                    }
+                    else
+                    {
+                        wordsToReturn = wordsToReturn + " " + word.Substring(0, 1).ToUpper() + word.Substring(1);
+                    }
+                    counter = counter + 1;
+                }
+
+                wordsToReturn = wordsToReturn.Trim();
+            }
+        }
+    }
+}
