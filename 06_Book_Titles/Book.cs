e1fba366a8c469b8a1cd633ff3da1ce5a59642ed
davi7725 <davi7725@edu.eal.dk>
2016-09-19 11:40:45 +0200

    Cleaned up the code and added comments

--- 06_Book_Titles/Book.cs
+++ 06_Book_Titles/Book.cs
@@ -2,64 +2,105 @@
 {
     internal class Book
     {
-        private string wordsToReturn = "";
+
+        //Backing variable for the property Title
+        private string title = "";
+        
         public string Title
         {
+
+            //Will return the variable when we access the property
             get
             {
-                return wordsToReturn;
+                return title;
             }
+
+            //Will set the variable when we assign a value to it
             internal set
             {
+
+                //create a string array with all of the words from the string
                 string[] words = value.Split();
+
+                //this variable will count how many iterations we have in the foreach
                 int counter = 0;
 
+                //go through all the words in the words array using the variable word
                 foreach(string word in words)
                 {
-                    if(0 != counter)
+                    //this line checks if this is the first iteration of the loop
+                    if(counter == 0)
                     {
-                        if ("the" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word;
-                        }
-                        else if ("a" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word;
-                        }
-                        else if ("an" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word;
-                        }
-                        else if ("and" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word;
-                        }
-                        else if ("in" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word;
-                        }
-                        else if ("of" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word;
-                        }
-                        else if ("i" == word)
-                        {
-                            wordsToReturn = wordsToReturn + " " + word.ToUpper();
-                        }
-                        else
-                        {
-                            wordsToReturn = wordsToReturn + " " + word.Substring(0, 1).ToUpper() + word.Substring(1);
-                        }
+                        //since this is the first iteration we need to uppercase it everytime
+                        //we get the first letter with substring and uppercase it and then add it the rest of the word
+                        title = title + " " + word.Substring(0, 1).ToUpper() + word.Substring(1);
                     }
                     else
                     {
-                        wordsToReturn = wordsToReturn + " " + word.Substring(0, 1).ToUpper() + word.Substring(1);
+                        //since this is not the first iteration anymore we need to check which words to capitalize
+                        //takeCareOfTheWord returns the string which will be already taken care of
+                        title = title + " " + takeCareOfTheWord(word);
                     }
+
+                    //add one unit to the counter variable for each iteration
                     counter = counter + 1;
                 }
 
-                wordsToReturn = wordsToReturn.Trim();
+                //assign the title variable but first trim it, so it takes out the end spaces and the start spaces.
+                title = title.Trim();
             }
         }
+
+        public string takeCareOfTheWord(string wordToTest)
+        {
+            //string array of words we should not capitalize
+            string[] notToCapitalize = { "and", "the", "or", "a", "an", "in", "of" };
+
+            //create a string to return at the end of the method
+            string finalWord = "";
+
+            //we need to have a boolean to see if we already changed the word on not
+            bool changed = false;
+
+            //is the wordToTest equals to i?
+            if(wordToTest == "i")
+            {
+                //Since it is we need to turn it to Upper
+                finalWord = wordToTest.ToUpper();
+
+                //change the value of changed to true, since we changed the finalWord var right now.
+                changed = true;
+            }
+
+            //this foreach loop will go through every word not to capitalize
+            foreach (string wordNotToCapitalize in notToCapitalize)
+            {
+                //is our word one of the ones we dont want to capitalize??
+               if(wordNotToCapitalize == wordToTest)
+                {
+
+                    //OH, it is!
+                    //So lets just assign it the wordToTest value since we need it the original way (probably lowecased)
+                    finalWord = wordToTest;
+
+                    //change the value of changed to true, since we changed the finalWord var right now.
+                    changed = true;
+                }
+            }
+
+
+            //Oh we got it without changing the variable?
+            //This is not one the words we need to change then, we should just uppercase it.
+            if(changed == false)
+            {
+                //we get the first letter with substring and uppercase it and then add it the rest of the word
+                finalWord = wordToTest.Substring(0, 1).ToUpper() + wordToTest.Substring(1);
+            }
+
+            //Lets just return this word which is now taken care of.
+            return finalWord;
+
+        }
+
     }
 }
